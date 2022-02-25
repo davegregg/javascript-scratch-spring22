4ec975d62d831b72f4dd5b71cadc81fb4e88a577
davegregg@gmail.com <davegregg@gmail.com>
2022-02-25 13:09:42 -0500

    Demo Code for Topic Session

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+console.log("Hey, it works!")
+
+// What are the data types in JavaScript?
+
+// PRIMITIVE DATA TYPES:
+//   Number: 0, 1, 4236534267, 4.45, -65, Infinity, NaN
+//   String: "hello", '32458 beers on the wall', `bat`, " ", ""
+//   Boolean: true, false
+//   undefined
+//   null
+// COMPLEX DATA TYPES:
+//   Object
+
+
+
+let currentPlayer
+currentPlayer = 3403
+console.log({currentPlayer})
+
+let nextPlayer = 1245
+
+// Next turn:
+let temp = currentPlayer
+currentPlayer = nextPlayer
+nextPlayer = temp
