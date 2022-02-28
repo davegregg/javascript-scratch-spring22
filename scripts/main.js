8d4221314d94951bbf292405da12407d3359ff2a
davegregg@gmail.com <davegregg@gmail.com>
2022-02-28 12:12:10 -0500

    Demo code, Block 1, Day 1

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ console.log("Hey, it works!")
 
 let currentPlayer
 currentPlayer = 3403
-console.log({currentPlayer})
+// console.log({currentPlayer})
 
 let nextPlayer = 1245
 
@@ -23,3 +23,78 @@ let nextPlayer = 1245
 let temp = currentPlayer
 currentPlayer = nextPlayer
 nextPlayer = temp
+
+
+
+let sum = function (x, y, z) {
+    return x + y + z
+}
+
+let printPaper = function (electricity, paper, ink, documentData) { // Receiving
+    // ...
+    let printedPage = "whatever"
+
+    return printedPage // Outbound (OTR)
+}
+
+let passThrough = function (value) {
+    console.log(value)
+    return value
+}
+
+let output = passThrough(3)
+console.log(output)
+
+let log = function (value) {
+    console.log("The value is: " + value)
+
+    return value
+}
+
+let result = log("batman")
+console.log(result)
+// console.log(log)
+
+let page = printPaper(145, "A4", "black", "<html></html>")
+console.log(page)
+
+console.log(printPaper)
+
+// run a function
+// call a function
+// invoke a function
+// execute a function
+// evaluate a function
+// "perform" a function
+// "hitting start on a printer"
+
+
+let warehouse = function (items, workers, electricity, food, water, equipment) { // Receiving
+    // ...
+    // ...
+
+    let packages = []
+    return packages // Outbound (OTR)
+}
+
+warehouse()
+
+
+
+let greetingIntroduction = "Good morning, "
+
+let greet = function (name) {
+    let message = greetingIntroduction + name
+    return message
+}
+
+
+// Given the input "Joseph",
+// function should output the string "Good morning, Joseph"
+console.assert(greet("Joseph") === "Good morning, Joseph")
+
+// Given the input "",
+// function should output the string "Good morning, "
+console.assert(greet("") === "Good morning, ")
+
+// "edge cases"
